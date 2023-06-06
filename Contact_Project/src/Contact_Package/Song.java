c506bb672a2757ec9f4d94eaf9bcc7e20c80f0e4
Dekel23 <121293804+Dekel23@users.noreply.github.com>
2023-06-06 16:56:50 +0300

    adding Media app - (media,song,video,mediaApp)
    I did my part of the media, classes - media, song, video, mediaApp.
    mediaApp is the overall app there is a main function but it was temporary u can delete it...

---
+++ Contact_Project/src/Contact_Package/Song.java
@@ -0,0 +1,9 @@
+package Contact_Package;
+
+public class Song extends Media {
+	public Song(double length, String name) { super(length, name);}
+
+	public String playing() {
+		return ("the song that's playing is" + super.getName() + " and his length is" + super.getLength());
+	}
+}
