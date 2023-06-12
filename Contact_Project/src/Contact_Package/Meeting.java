174e74238cab8fbad48fa4665cb836931fbbf880
barzomer <134528027+barzomer@users.noreply.github.com>
2023-06-13 01:24:41 +0300

    Update Meeting.java

--- Contact_Project/src/Contact_Package/Meeting.java
+++ Contact_Project/src/Contact_Package/Meeting.java
@@ -2,13 +2,22 @@ package Contact_Package;
 import java.util.Date;
 public class Meeting extends Event {
 	private Contact contact;
-	public Meeting(String name, Date date, int len, Contact contact)
+	public Meeting(Date date, int len, Contact contact)
 	{
-		super(name, date, len);
+		super(date, len);
 		this.setContact(contact);
 	}
 	public void setContact(Contact contact)
 	{
 		this.contact = contact;
+	}
+	public Contact getContact()
+	{
+		return this.contact;
+	}
+	public void print()
+	{
+		super.print();
+		System.out.println("meeting with: " + contact.getName());
 	}
 }
