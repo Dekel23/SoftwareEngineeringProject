56a9d114fede227379898b3c5d26d7e26c2858b9
barzomer <134528027+barzomer@users.noreply.github.com>
2023-06-13 01:25:36 +0300

    Update Event.java

--- Contact_Project/src/Contact_Package/Event.java
+++ Contact_Project/src/Contact_Package/Event.java
@@ -3,33 +3,33 @@ import java.util.Date;
 import java.util.Scanner;
 public class Event {
 	public static Scanner in = new Scanner (System.in);
-	private String name;
 	private Date date;
 	private int len;
-	public Event(String name, Date date, int len)
+	public Event(Date date, int len)
 	{
 		this.setDate(date);
 		this.setLen(len);
-		this.setName(name);
 	}
-	public void setName(String name)
-	{
-		this.name=name;
-	}
+
 	public void setDate(Date date)
 	{
 		this.date = date;
+	}
+	public Date getDate()
+	{
+		return this.date;
 	}
 	public void setLen(int len)
 	{
 		this.len=len;
 	}
-	public Date getDate()
-	{
-		return date;
-	}
 	public int getLen()
 	{
-		return len;
+		return this.len;
+	}
+	public void print()
+	{
+		System.out.println(date.toLocaleString());
+		System.out.println("event length: " + len + "minutes");
 	}
 }
