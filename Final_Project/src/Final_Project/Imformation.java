1939d31719731f7f2c606a42e32a90c9a92d156a
Dekel23 <121293804+Dekel23@users.noreply.github.com>
2023-07-19 12:09:50 +0300

    Delete Final_Project/src/Final_Project directory

--- Final_Project/src/Final_Project/Imformation.java
+++
@@ -1,9 +0,0 @@
-package Final_Project;
-
-public interface Imformation {
-	public void printEvents();
-	public void printSensors();
-	public void addSecsor();
-	public void deleteSensor();
-	public void chargeSesor();
-}
